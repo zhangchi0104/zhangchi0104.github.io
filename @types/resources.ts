f59e8a0dda861cedb6047551c07af08a600b3956
Alex Zhang <zhangchi0104@live.com>
2023-11-13 18:01:31 +1100

    chore: added i18next typing

--- @types/resources.ts
+++
@@ -1,7 +0,0 @@
-import translations from '../langs/en/translations.json';
-
-const resources = {
-  translations
-} as const;
-
-export default resources;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./main.css";
-import "./i18n";
+import i18next from "./i18n";
 import { Provider } from "react-redux";
 import { store } from "./store";
-
 const elem = document.getElementById("app");
+i18next.t("");
 if (elem != null) {
   const root = createRoot(elem);
   root.render(
