0d9f6f94a68464d7c2db8926c91380f3fba595b5
Alex Zhang <39239965+zhangchi0104@users.noreply.github.com>
2025-07-09 10:08:08 +1000

    fix: gears button

--- src/components/NavBar/LanguageSelector.tsx
+++ src/components/NavBar/LanguageSelector.tsx
@@ -5,83 +5,83 @@ import GearsIcon from "../icons/GearIcon";
 import ToggleSwitch from "../ToggleSwtich";
 
 const LanguageSelector: React.FC = () => {
-  const { i18n } = useTranslation();
-  const locale = i18n.language;
-  const [dropdownVisibility, setDropdownVisibility] = useState(true);
-  const anchorRef = useRef<HTMLDivElement>(null);
-  const setLanguage = (lang: string) => {
-    if (lang === locale) {
-      return;
-    }
-    i18n.changeLanguage(lang);
-  };
-  const toggleDropdown = () => {
-    setDropdownVisibility(!dropdownVisibility);
-  };
-  const onLangListClick = (ev: React.MouseEvent<HTMLUListElement>) => {
-    const target = ev.target as HTMLLIElement;
-    if (target.dataset.value && target.dataset.value !== locale) {
-      setLanguage(target.dataset.value);
-    }
-  };
+	const { i18n } = useTranslation();
+	const locale = i18n.language;
+	const [dropdownVisibility, setDropdownVisibility] = useState(true);
+	const anchorRef = useRef<HTMLDivElement>(null);
+	const setLanguage = (lang: string) => {
+		if (lang === locale) {
+			return;
+		}
+		i18n.changeLanguage(lang);
+	};
+	const toggleDropdown = () => {
+		setDropdownVisibility(!dropdownVisibility);
+	};
+	const onLangListClick = (ev: React.MouseEvent<HTMLUListElement>) => {
+		const target = ev.target as HTMLLIElement;
+		if (target.dataset.value && target.dataset.value !== locale) {
+			setLanguage(target.dataset.value);
+		}
+	};
 
-  const iconAnimation = "animate-spin-bounce duration-300";
-  return (
-    <>
-      <div onClick={toggleDropdown} ref={anchorRef}>
-        <GearsIcon
-          className={`hover:cursor-pointer relative ${
-            dropdownVisibility ? iconAnimation : ""
-          }`}
-        />
-      </div>
-      {anchorRef.current && (
-        <Dropdown
-          anchor={anchorRef.current}
-          visible={dropdownVisibility}
-          className="w-52 right-0 py-4 mr-1"
-        >
-          <div>
-            <ul
-              className="text-gray-900 text-sm px-4"
-              onClick={onLangListClick}
-            >
-              <li
-                className={`${locale === "zh" ? "font-bold text-sky-900" : ""}`}
-                data-value={"zh"}
-              >
-                简体中文
-              </li>
-              <li
-                data-value="en"
-                className={`${
-                  locale === "en" ? "font-bold text-sky-900" : ""
-                } mt-3`}
-              >
-                English
-              </li>
-              <li
-                data-value="arisu"
-                className={`${
-                  locale.includes("arisu") ? "font-bold text-sky-900" : ""
-                } mt-3`}
-              >
-                {locale.includes("zh") ? "邦邦咔邦" : "Ban-Ban-Ka-Ban"}
-              </li>
-            </ul>
-            <hr className="mt-3" />
-            <div className="text-gray-900 text-sm px-4">
-              <div className="flex justify-between items-center mt-3">
-                <p>Fancy Mode</p>
-                <ToggleSwitch></ToggleSwitch>
-              </div>
-            </div>
-            <hr className="mt-3" />
-          </div>
-        </Dropdown>
-      )}
-    </>
-  );
+	const iconAnimation = "animate-spin-bounce duration-300";
+	return (
+		<>
+			<div onClick={toggleDropdown} ref={anchorRef}>
+				<GearsIcon
+					className={`hover:cursor-pointer relative ${
+						dropdownVisibility ? iconAnimation : ""
+					}`}
+				/>
+			</div>
+			{anchorRef.current && (
+				<Dropdown
+					anchor={anchorRef.current}
+					visible={dropdownVisibility}
+					className="w-52 right-0 py-4 mr-1"
+				>
+					<div>
+						<ul
+							className="text-gray-900 text-sm px-4"
+							onClick={onLangListClick}
+						>
+							<li
+								className={`${locale === "zh" ? "font-bold text-sky-900" : ""}`}
+								data-value={"zh"}
+							>
+								简体中文
+							</li>
+							<li
+								data-value="en"
+								className={`${
+									locale === "en" ? "font-bold text-sky-900" : ""
+								} mt-3`}
+							>
+								English
+							</li>
+							<li
+								data-value="arisu"
+								className={`${
+									locale.includes("arisu") ? "font-bold text-sky-900" : ""
+								} mt-3`}
+							>
+								{locale.includes("zh") ? "邦邦咔邦" : "Ban-Ban-Ka-Ban"}
+							</li>
+						</ul>
+						<hr className="mt-3" />
+						<div className="text-gray-900 text-sm px-4">
+							<div className="flex justify-between items-center mt-3">
+								<p>Fancy Mode</p>
+								<ToggleSwitch></ToggleSwitch>
+							</div>
+						</div>
+						<hr className="mt-3" />
+					</div>
+				</Dropdown>
+			)}
+		</>
+	);
 };
 
 export default LanguageSelector;
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -8,74 +8,74 @@ import { selectScrolled } from "@/store/selectors";
 type ItemSelectCallback = (name: SectionName) => void;
 const animation = "transition-[box-shadow]";
 const MobileNavBar: React.FC<{ showShadow: boolean }> = ({ showShadow }) => {
-  const { t } = useTranslation();
-  const shawdow = showShadow
-    ? "shadow-md mb-4 bg-amber-50"
-    : "shadow-none bg-transparent";
-  return (
-    <div
-      className={`${animation} flex justify-between w-screen py-2 px-4 fixed items-center bg-amber-50 dark:bg-slate-900 top-0 ${shawdow}`}
-    >
-      <div className="text-2xl font-semibold text-gray-700 dark:text-slate-300 text-center">
-        {t("navbar.name")}
-      </div>
-      <Settings />
-    </div>
-  );
+	const { t } = useTranslation();
+	const shawdow = showShadow
+		? "shadow-md mb-4 bg-amber-50"
+		: "shadow-none bg-transparent";
+	return (
+		<div
+			className={`${animation} flex justify-between w-screen py-2 px-3 fixed items-center bg-amber-50 dark:bg-slate-900 top-0 left-0 ${shawdow}`}
+		>
+			<div className="text-2xl font-semibold text-gray-700 dark:text-slate-300 text-center">
+				{t("navbar.name")}
+			</div>
+			<Settings />
+		</div>
+	);
 };
 const DesktopNavBar: React.FC<{
-  onClick: ItemSelectCallback;
-  showShadow: boolean;
+	onClick: ItemSelectCallback;
+	showShadow: boolean;
 }> = ({ onClick, showShadow = false }) => {
-  const { t } = useTranslation();
-  const shadow = showShadow ? "shadow-md" : "shadow-none";
+	const { t } = useTranslation();
+	const shadow = showShadow ? "shadow-md" : "shadow-none";
 
-  return (
-    <nav
-      className={`${animation} fixed top-0 left-0 w-full py-2 pl-4 pr-4 bg-amber-50 dark:bg-slate-900  ${shadow}`}
-    >
-      <div className="mx-auto max-w-screen-lg flex flex-row md:justify-end items-center">
-        <div className="flex flex-row">
-          <NavBarItem
-            className="mr-8 md:mr-12 lg:mr-20"
-            name="home"
-            onClick={onClick}
-          >
-            {t("navbar.home")}
-          </NavBarItem>
-          <NavBarItem
-            className="mr-8 md:mr-12 lg:mr-20"
-            name="about-me"
-            onClick={onClick}
-          >
-            {t("navbar.aboutMe")}
-          </NavBarItem>
-          <NavBarItem
-            className="mr-8 md:mr-12 lg:mr-20"
-            name="projects"
-            onClick={onClick}
-          >
-            {t("navbar.projects")}
-          </NavBarItem>
-        </div>
-        <Settings />
-      </div>
-    </nav>
-  );
+	return (
+		<nav
+			className={`${animation} fixed top-0 left-0 w-screen py-2 pl-4 pr-4 bg-amber-50 dark:bg-slate-900  ${shadow}`}
+		>
+			<div className="mx-auto max-w-screen-lg flex flex-row md:justify-end items-center">
+				<div className="flex flex-row">
+					<NavBarItem
+						className="mr-8 md:mr-12 lg:mr-20"
+						name="home"
+						onClick={onClick}
+					>
+						{t("navbar.home")}
+					</NavBarItem>
+					<NavBarItem
+						className="mr-8 md:mr-12 lg:mr-20"
+						name="about-me"
+						onClick={onClick}
+					>
+						{t("navbar.aboutMe")}
+					</NavBarItem>
+					<NavBarItem
+						className="mr-8 md:mr-12 lg:mr-20"
+						name="projects"
+						onClick={onClick}
+					>
+						{t("navbar.projects")}
+					</NavBarItem>
+				</div>
+				<Settings />
+			</div>
+		</nav>
+	);
 };
 const NavBar: React.FC<{ onClick: ItemSelectCallback }> = ({ onClick }) => {
-  const isMobile = useMediaQuery("(min-width: 640px)");
-  const scrolled = useAppSelector(selectScrolled);
+	const isMobile = useMediaQuery("(min-width: 640px)");
+	const scrolled = useAppSelector(selectScrolled);
 
-  return (
-    <div>
-      {!isMobile ? (
-        <MobileNavBar showShadow={scrolled} />
-      ) : (
-        <DesktopNavBar showShadow={scrolled} onClick={onClick} />
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{!isMobile ? (
+				<MobileNavBar showShadow={scrolled} />
+			) : (
+				<DesktopNavBar showShadow={scrolled} onClick={onClick} />
+			)}
+		</div>
+	);
 };
 
 export default NavBar;
